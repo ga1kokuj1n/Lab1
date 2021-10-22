aa56c74091e6b76d7166eaa66c70497882909462
ga1kokuj1n <92709973+ga1kokuj1n@users.noreply.github.com>
2021-10-22 05:06:25 +0300

    Add files via upload

---
+++ Lab1/Lab1.7/Lab1.7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Int32.Parse(Console.ReadLine());
+            while (num < 100 || num > 999)
+            {
+                Console.WriteLine("Некорректные данные! Повторите ввод!");
+                num = Int32.Parse(Console.ReadLine());
+            }
+
+            int reversed = (num / 100) + (num / 10 % 10 * 10) + (num % 10 * 100);
+            Console.WriteLine(reversed);
+        }
+    }
+}
