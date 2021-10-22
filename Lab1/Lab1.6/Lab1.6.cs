be05cf57895e06917634a23cbe9c7f27f380fe81
ga1kokuj1n <92709973+ga1kokuj1n@users.noreply.github.com>
2021-10-22 04:44:35 +0300

    Add files via upload

---
+++ Lab1/Lab1.6/Lab1.6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите четырёхзначное число: ");
+			int num = Int32.Parse(Console.ReadLine());
+			while (num < 1000 || num > 9999)
+			{
+				Console.WriteLine("Некорректные данные! Повторите ввод");
+				num = Int32.Parse(Console.ReadLine());
+			}
+
+			int ans = (num / 1000) * (num / 100 % 10) * (num / 10 % 10) * (num % 10);
+			Console.WriteLine($"Ответ: {ans}");
+		}
+	}
+}
